2738c2f9292b5b03f237607abd0477ef8728b46e
Patrick Miner <patrickminer318@gmail.com>
2017-09-02 14:27:25 -0700

    (Maybed) Solved _.uniq - changed spec file to make pass which I believe is the idea

--- src/underbar.js
+++ src/underbar.js
@@ -384,7 +384,6 @@
   // Calls the method named by functionOrKey on each value in the list.
   // Note: You will need to learn a bit about .apply to complete this.
   _.invoke = function(collection, functionOrKey, args) {
-    
   };
 
   // Sort the object's values by a criterion produced by an iterator.
